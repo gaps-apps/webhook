d491e8df95dd53d2229e952573bced9b80817f1e
fuzz88 <ivan@oschepkov.ru>
2022-11-20 17:37:06 +0800

    add sample gh webhook test

--- app/app.py
+++ app/app.py
@@ -1,13 +1,12 @@
 from sanic import Sanic
 from sanic.response import text
-from sanic.log import logger
+
 
 def create_app():
     app = Sanic("webhook")
 
     @app.post("/gh")
     async def from_github(request):
-        logger.info(request.body)
         return text("ok")
 
     return app
