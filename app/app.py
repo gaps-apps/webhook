25d5b86f88560fcd8334ed514c05e383a913caea
fuzz88 <ivan@oschepkov.ru>
2022-11-20 18:47:00 +0800

    add signature

--- app/app.py
+++ app/app.py
@@ -1,12 +1,23 @@
 from sanic import Sanic
 from sanic.response import text
+from sanic.log import logger
+import os
+import hmac
+import hashlib
 
 
 def create_app():
     app = Sanic("webhook")
 
+    GH_WH_SECRET = os.getenv("GH_WH_SECRET", "")
+
     @app.post("/gh")
     async def from_github(request):
+        logger.info(request.headers["x-hub-signature-256"])
+        signature = hmac.new(
+            bytes(GH_WH_SECRET, "utf-8"), request.body, digestmod=hashlib.sha256
+        )
+        logger.info(f"sha256={signature}")
         return text("ok")
 
     return app
