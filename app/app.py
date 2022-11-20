1410d6b0db21898c99948c51bb0fc6b8570a69c2
fuzz88 <ivan@oschepkov.ru>
2022-11-20 17:27:41 +0800

    test logger

--- app/app.py
+++ app/app.py
@@ -1,12 +1,13 @@
 from sanic import Sanic
 from sanic.response import text
-
+from sanic.log import logger
 
 def create_app():
     app = Sanic("webhook")
 
     @app.post("/gh")
     async def from_github(request):
+        logger.info(request.body)
         return text("ok")
 
     return app
